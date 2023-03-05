97735b7010c9d86fc4b4888ca38cf0a34876a148
“zhuopf” <1707379815@qq.com>
2023-03-05 21:30:16 +0800

    homework

---
+++ no1.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+# 小舞中午在饭堂吃饭，饭价是18.5元，买了一瓶可乐价格3.5元，吃了一份水果10.7元，请定义变量表示小舞在饭堂吃饭的花费，并计算总价格，在屏幕输出最终价格。
+
+food_price = 18.5
+cola_price = 3.5
+fruit_price = 10.7
+print("总价格:", food_price+cola_price+fruit_price)
